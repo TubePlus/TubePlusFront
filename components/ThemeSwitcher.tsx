42dec89006c9b7023ae1fb5239bac4e91f7aaa6e
young1ll <122770896+young1ll@users.noreply.github.com>
2023-10-20 13:48:14 +0900

    FEAT System theme by default - session이 없을 때, 기본으로 systemTheme 적용 - systemTheme이 적용되었을 때, theme을 변경하면 해당 화면에서 더이상 systemTheme이 아닌 theme이 적용됨 - 새로고침 또는 relaod 시 다시 systemTheme 적용

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -2,23 +2,21 @@
 import { getAuthSession } from '@/lib/auth';
 import { Button, Skeleton, Spinner, Switch } from '@nextui-org/react';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useSession } from 'next-auth/react';
 import { useTheme } from 'next-themes';
 import { usePathname } from 'next/navigation';
 import React, { Suspense, useEffect, useState } from 'react';
 
 export const ThemeSwitcher = ({ type }: { type: 'toggle' | 'button' }) => {
-  const pathname = usePathname();
   const [mounted, setMounted] = useState(false); // hydration error 방지
   const { theme, setTheme, systemTheme } = useTheme(); // TODO: user를 위한 setThme() 사용 시 Handler 안에 넣어서 session 확인, 확인 후 post fetch
+  const { data: session } = useSession();
 
   useEffect(() => {
     setMounted(true);
-    async () => {
-      const session = await getAuthSession();
-      if (!session?.user) {
-        setTheme('system');
-      }
-    };
+    if (!session?.user) {
+      setTheme('system');
+    }
   }, []); // TODO: Warning 의존성 배열에 setTheme 필요 - "mount 시에만 실행되어야 하기 때문에 추가하면 안됨"
 
   switch (type) {
@@ -34,12 +32,6 @@ export const ThemeSwitcher = ({ type }: { type: 'toggle' | 'button' }) => {
             startContent={<SunIcon />}
             endContent={<MoonIcon />}
           />
-          {pathname === '/test' ? (
-            <div className="text-sm">
-              <p>System: {systemTheme}</p>
-              <p>Theme: {theme}</p>
-            </div>
-          ) : null}
         </>
       );
 
@@ -51,7 +43,17 @@ export const ThemeSwitcher = ({ type }: { type: 'toggle' | 'button' }) => {
             theme === 'light' ? setTheme('dark') : setTheme('light')
           }
         >
-          {theme === 'light' ? <SunIcon /> : <MoonIcon />}
+          {!session ? (
+            systemTheme === 'light' ? (
+              <SunIcon />
+            ) : (
+              <MoonIcon />
+            )
+          ) : theme === 'light' ? (
+            <SunIcon />
+          ) : (
+            <MoonIcon />
+          )}
         </Button>
       ) : (
         <div>
