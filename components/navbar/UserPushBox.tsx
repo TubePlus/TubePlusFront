2624e47f3660da077ec24372ea39a5bd20069110
young1ll <122770896+young1ll@users.noreply.github.com>
2023-11-08 18:55:38 +0900

    FEAT add dropdown

--- components/navbar/UserPushBox.tsx
+++ components/navbar/UserPushBox.tsx
@@ -3,7 +3,59 @@ import { NavbarItem } from '@nextui-org/navbar';
 import { Button } from '@nextui-org/button';
 import { BellIcon } from '@radix-ui/react-icons';
 import { useEffect, useState } from 'react';
-import { Skeleton } from '@nextui-org/react';
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Skeleton,
+} from '@nextui-org/react';
+
+const pushTestDatas = [
+  //TODO: eventSource 사용해서 sse 구현하기
+  {
+    uuid: 0,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+  {
+    uuid: 1,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+  {
+    uuid: 2,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+  {
+    uuid: 3,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+  {
+    uuid: 4,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+  {
+    uuid: 5,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+  {
+    uuid: 6,
+    alarm_title: 'test alarm',
+    alarm_content: 'this is test',
+    autor_name: 'admin',
+  },
+];
 
 const UserPushBox = () => {
   const [mounted, setMounted] = useState(false);
@@ -15,9 +67,24 @@ const UserPushBox = () => {
   return mounted ? (
     <>
       <NavbarItem>
-        <Button variant="light" isIconOnly radius="full">
-          <BellIcon />
-        </Button>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant="light" isIconOnly radius="full">
+              <BellIcon />
+            </Button>
+          </DropdownTrigger>
+
+          <DropdownMenu aria-label="Push Alarms">
+            {pushTestDatas.map(item => (
+              <DropdownItem key={item.uuid}>
+                <div className="flex flex-col">
+                  {item.alarm_title}
+                  <small className="line-clamp-1">{item.alarm_content}</small>
+                </div>
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
       </NavbarItem>
     </>
   ) : (
