cca4f56a358cea90876778c168216c1e4ced43b6
young1ll <122770896+young1ll@users.noreply.github.com>
2023-11-20 14:55:50 +0900

    CHORE tailwind-scrollbar rounded

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     require('tailwindcss-animate'),
-    require('tailwind-scrollbar'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
     nextui(),
   ],
 };
