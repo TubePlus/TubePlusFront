00667d71f32821aace387ce5ba53a5ef3b999e06
young1ll <122770896+young1ll@users.noreply.github.com>
2023-11-21 17:18:10 +0900

    FIX i18n generateStatics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -55,6 +55,7 @@ export default async function LocaleLayout({
   } catch (error) {
     notFound();
   }
+  unstable_setRequestLocale(locale);
 
   return (
     <html lang={locale} suppressHydrationWarning>
