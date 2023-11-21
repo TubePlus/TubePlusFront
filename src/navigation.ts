bf2287a4e5a3fa58b3ebd01d4c9107ea93abb3f9
young1ll <122770896+young1ll@users.noreply.github.com>
2023-11-21 23:10:17 +0900

    FEAT Add i18n dictionaries

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,5 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
-export const locales = ['en', 'ko'] as const;
+export const locales = ['en', 'ko', 'ja', 'zh', 'es'] as const;
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales });
